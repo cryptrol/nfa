f051d661f673baa5b7f8df169117039db9b06ce3
cryptrol <0000@gmx.ca>
2016-01-03 16:09:01 +0100

    Added filter by allowed extension, removed no-auth smtp option

--- nfa.go
+++ nfa.go
@@ -3,8 +3,8 @@ package main
 import (
 	"github.com/rjeczalik/notify"
 	"github.com/spf13/viper"
-	"log"
 	"path"
+	"log"
 	"net/smtp"
 	"flag"
     "github.com/scorredoira/email"
@@ -15,17 +15,27 @@ const (
 	BODY = "This is the message body."
 )
 
+// Used to search in the available file extensions slice
+func SliceContainsString(slice []string, s string) bool {
+    for _, i := range slice {
+        if i == s {
+            return true
+        }
+    }
+    return false
+}
+
 func main() {
 	var config string
-	flag.StringVar(&config, "config", "nfa.toml", "The name of the config file.")
+	flag.StringVar(&config, "config", "nfa", "The name of the config file (withour the toml extension.")
 	flag.Parse()
-	vconfig := config[:len(config)-len(path.Ext(config))] // strip extension
-	viper.SetConfigName(vconfig) 
+	viper.SetConfigName(config) 
 	viper.AddConfigPath(".")
+	viper.AddConfigPath("/etc/nfa")
 	err := viper.ReadInConfig()
 	if err != nil { // Handle errors reading the config file
-		log.Fatal("Fatal error config file: ", err)
-	    panic("panic")
+		log.Fatal("Fatal error reading config file.")
+	    panic(err)
 	}
 	// Make the channel buffered to ensure no event is dropped. Notify will drop
 	// an event if the receiver is not able to keep up the sending pace.
@@ -41,22 +51,27 @@ func main() {
 		defer notify.Stop(c)
 		ei := <-c
 		log.Println("Got event:", ei)
-		// if ei.isCreate() {
-			m := email.NewMessage(viper.GetString("mail.subject"), viper.GetString("mail.body"))
-			m.From = viper.GetString("mail.from")
-			m.To = viper.GetStringSlice("mail.to") // should be []string{"a", "b", ... }
-			log.Println("About to attach : " + ei.Path())
-			err := m.Attach(ei.Path())
-			if err != nil {
-			    log.Println(err)
+		if ei.Event() == notify.Create {
+			if SliceContainsString(viper.GetStringSlice("app.extensions"), path.Ext(ei.Path())[1:]) {
+				m := email.NewMessage(viper.GetString("mail.subject"), viper.GetString("mail.body"))
+				m.From = viper.GetString("mail.from")
+				m.To = viper.GetStringSlice("mail.to") // should be []string{"a", "b", ... }
+				log.Println("About to attach : " + ei.Path())
+				err := m.Attach(ei.Path())
+				if err != nil {
+				    log.Println(err)
+				}
+				auth := smtp.PlainAuth("", viper.GetString("mail.login"), viper.GetString("mail.password"), viper.GetString("mail.server"))
+				err = email.Send(viper.GetString("mail.server") + ":" + viper.GetString("mail.port"), auth, m)
+				if err != nil {
+					log.Println("Can't send mail : ", err)
+				}
+			} else {
+				log.Println("Ignoring created file with disallowed extension.")
 			}
-			err = email.Send(viper.GetString("mail.server") + ":" + viper.GetString("mail.port"), smtp.PlainAuth("", viper.GetString("mail.login"), viper.GetString("mail.password"), viper.GetString("mail.server")), m)
-			if err != nil {
-				log.Println("Can't send mail : ", err)
-			}
-		// } else {
-		//	log.Println("Received a not created file event, ignoring.")
-		//}
+		} else {
+			log.Println("Ignoring non create event.")
+		}
 	}
 
 
